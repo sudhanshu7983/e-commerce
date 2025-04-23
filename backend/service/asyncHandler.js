638a00179efa068e0f2b27e86ceaca2b8a8d644b
sudhanshu pathak <pji101329@gmail.com>
2025-04-23 21:43:37 +0530

    commit

--- backend/service/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next)
-    } catch (error) {
-        console.log(error)
-        res.json({status:false,message:error})
-    }
-}
-module.exports = {asyncHandler};
